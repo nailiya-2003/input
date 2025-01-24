5ced5652807aafe6a7dd50d692ce886cbdcd7b75
Naila <naile.gashimova@gmail.com>
2025-01-25 00:47:03 +0400

    edit

--- mobile.js
+++ mobile.js
@@ -6,8 +6,6 @@ class Paper {
   touchStartY = 0;
   touchMoveX = 0;
   touchMoveY = 0;
-  touchEndX = 0;
-  touchEndY = 0;
   prevTouchX = 0;
   prevTouchY = 0;
   velX = 0;
@@ -20,57 +18,48 @@ class Paper {
   init(paper) {
     paper.addEventListener('touchmove', (e) => {
       e.preventDefault();
-      if(!this.rotating) {
-        this.touchMoveX = e.touches[0].clientX;
-        this.touchMoveY = e.touches[0].clientY;
-        
-        this.velX = this.touchMoveX - this.prevTouchX;
-        this.velY = this.touchMoveY - this.prevTouchY;
-      }
-        
-      const dirX = e.touches[0].clientX - this.touchStartX;
-      const dirY = e.touches[0].clientY - this.touchStartY;
-      const dirLength = Math.sqrt(dirX*dirX+dirY*dirY);
-      const dirNormalizedX = dirX / dirLength;
-      const dirNormalizedY = dirY / dirLength;
+      if (!this.holdingPaper) return;
 
-      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
-      let degrees = 180 * angle / Math.PI;
-      degrees = (360 + Math.round(degrees)) % 360;
-      if(this.rotating) {
-        this.rotation = degrees;
-      }
+      this.touchMoveX = e.touches[0].clientX;
+      this.touchMoveY = e.touches[0].clientY;
 
-      if(this.holdingPaper) {
-        if(!this.rotating) {
-          this.currentPaperX += this.velX;
-          this.currentPaperY += this.velY;
-        }
-        this.prevTouchX = this.touchMoveX;
-        this.prevTouchY = this.touchMoveY;
+      // Вычисляем изменение координат (скорость)
+      this.velX = this.touchMoveX - this.prevTouchX;
+      this.velY = this.touchMoveY - this.prevTouchY;
 
-        paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
-      }
-    })
+      // Обновляем позицию бумаги в соответствии с движением пальца
+      this.currentPaperX += this.velX;
+      this.currentPaperY += this.velY;
+
+      // Применяем стили (перемещение и вращение)
+      paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
+
+      // Обновляем предыдущие координаты
+      this.prevTouchX = this.touchMoveX;
+      this.prevTouchY = this.touchMoveY;
+    });
 
     paper.addEventListener('touchstart', (e) => {
-      if(this.holdingPaper) return; 
+      if (this.holdingPaper) return;
       this.holdingPaper = true;
-      
+
+      // Повышаем z-index для того, чтобы картинка была выше всех других
       paper.style.zIndex = highestZ;
       highestZ += 1;
-      
+
+      // Начальные координаты для отслеживания движения пальца
       this.touchStartX = e.touches[0].clientX;
       this.touchStartY = e.touches[0].clientY;
       this.prevTouchX = this.touchStartX;
       this.prevTouchY = this.touchStartY;
     });
+
     paper.addEventListener('touchend', () => {
       this.holdingPaper = false;
       this.rotating = false;
     });
 
-    // For two-finger rotation on touch screens
+    // Для двухпальцевого вращения на сенсорных экранах
     paper.addEventListener('gesturestart', (e) => {
       e.preventDefault();
       this.rotating = true;
